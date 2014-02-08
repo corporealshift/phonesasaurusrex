49a0b1891411a48d6cff1c2c1f3a604a7b90f057
Lucas Kam <lkam@picsauditing.com>
2014-02-07 20:40:41 -0800

    Changed Login to return JSON
    url: /Login.action?userName={USER_NAME}&password{PASSWORD}
    
    result: "success" or "failure"

--- src/net/feminaexlux/struts2/minimal/actions/Login.java
+++ src/net/feminaexlux/struts2/minimal/actions/Login.java
@@ -10,30 +10,30 @@ public class Login extends DefaultActionSupport{
 
     private String password;
 
+    private String result;
+
    public String execute() {
+       result = "failure";
        if (userName != null) {
             User user = UserDao.findByUserName(userName);
 
             if (user != null && password != null && password.equals(user.getPassword())) {
-                return SUCCESS;
+                result = "success";
             }
        }
-       return INPUT;
-   }
 
-    public String getUserName() {
-        return userName;
-    }
+       return SUCCESS;
+   }
 
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getResult() {
+        return result;
     }
 }
