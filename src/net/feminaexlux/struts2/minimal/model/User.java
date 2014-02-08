45ec8098bca4b3402bd9736d5250b71e56f744a5
Lani Aung <github@laniaung.me>
2014-02-07 20:05:01 -0800

    Adding spring context

--- src/net/feminaexlux/struts2/minimal/model/User.java
+++ src/net/feminaexlux/struts2/minimal/model/User.java
@@ -1,39 +1,50 @@
 package net.feminaexlux.struts2.minimal.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
-    public String userName;
 
-    public String password;
+	@Column(name = "user_name")
+	public String userName;
 
-    public int score;
+	public String password;
 
-    public User(String userName, String password, int score) {
-        this.userName = userName;
-        this.password = password;
-        this.score = score;
-    }
+	public int score;
 
-    public String getUserName() {
-        return userName;
-    }
+	public User() {
+	}
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+	public User(String userName, String password, int score) {
+		this.userName = userName;
+		this.password = password;
+		this.score = score;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getUserName() {
+		return userName;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-    public int getScore() {
-        return score;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setScore(int score) {
-        this.score = score;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
 }
